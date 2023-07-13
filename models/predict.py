56bd7490a68382ed0d633239df4b86068b05b473
armani-8 <44300454+armani-8@users.noreply.github.com>
2023-07-13 11:25:07 -0400

    Update predict.py

--- models/predict.py
+++ models/predict.py
@@ -4,15 +4,16 @@ import sys
 
 # Load the model and encoders
 model = joblib.load('salary_model.pkl')
-label_encoder = joblib.load('label_encoder.pkl')
+job_role_encoder = joblib.load('job_role_encoder.pkl')
+work_location_encoder = joblib.load('work_location_encoder.pkl')
 
 # Read inputs from the command line
 jobRole = sys.argv[1]
 workLocation = sys.argv[2]
 
 # Perform label encoding
-jobRole_encoded = label_encoder.transform([jobRole])
-workLocation_encoded = label_encoder.transform([workLocation])
+jobRole_encoded = job_role_encoder.transform([jobRole])
+workLocation_encoded = work_location_encoder.transform([workLocation])
 
 # Create a DataFrame
 X = pd.DataFrame({
