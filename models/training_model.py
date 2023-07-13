896b66441a03cb7af5249db9ac8152cfd8d8ed4c
armani-8 <44300454+armani-8@users.noreply.github.com>
2023-07-13 09:53:48 -0400

    Create training_model.py

---
+++ models/training_model.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+
+# IMPORTANT :: Requires populated MongoDB collection 'employees' 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['hackathon_travelers']
+employees = db['employees']
+
+# Fetch data from collection
+data = []
+for employee in employees.find():
+    data.append({
+        'jobRole': employee['jobRole'],
+        'workLocation': employee['workLocation'],
+        'salary': employee['salary']
+    })
+
+# Create pandas DataFrame
+df = pd.DataFrame(data)
+
+# Encode categories
+label_encoder = LabelEncoder()
+df['jobRole'] = label_encoder.fit_transform(df['jobRole'])
+df['workLocation'] = label_encoder.fit_transform(df['workLocation'])
+
+# Split into features (X) and target variable (y)
+X = df[['jobRole', 'workLocation']]
+y = df['salary']
+
+# Create and train linear regression model
+model = LinearRegression()
+model.fit(X, y)
