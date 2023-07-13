1a9e03ff0f51df35e27b766ac8f87eee25136f6f
armani-8 <44300454+armani-8@users.noreply.github.com>
2023-07-13 10:04:30 -0400

    Update training_model.py

--- models/training_model.py
+++ models/training_model.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
+from pymongo import MongoClient
 
 # IMPORTANT :: Requires populated MongoDB collection 'employees' 
 client = MongoClient('mongodb://localhost:27017/')
@@ -20,9 +22,11 @@ for employee in employees.find():
 df = pd.DataFrame(data)
 
 # Encode categories
-label_encoder = LabelEncoder()
-df['jobRole'] = label_encoder.fit_transform(df['jobRole'])
-df['workLocation'] = label_encoder.fit_transform(df['workLocation'])
+jobRole_encoder = LabelEncoder()
+df['jobRole'] = jobRole_encoder.fit_transform(df['jobRole'])
+
+workLocation_encoder = LabelEncoder()
+df['workLocation'] = workLocation_encoder.fit_transform(df['workLocation'])
 
 # Split into features (X) and target variable (y)
 X = df[['jobRole', 'workLocation']]
@@ -31,3 +35,9 @@ y = df['salary']
 # Create and train linear regression model
 model = LinearRegression()
 model.fit(X, y)
+
+# Save model and encoders
+with open('model.pkl', 'wb') as model_file, open('jobRole_encoder.pkl', 'wb') as jobRole_file, open('workLocation_encoder.pkl', 'wb') as workLocation_file:
+    pickle.dump(model, model_file)
+    pickle.dump(jobRole_encoder, jobRole_file)
+    pickle.dump(workLocation_encoder, workLocation_file)
