0aefd27dc1b545c5e8c9bb9873ae2c2467d5f268
armani-8 <44300454+armani-8@users.noreply.github.com>
2023-07-14 09:54:51 -0400

    Update predict.py

--- models/predict.py
+++ models/predict.py
@@ -4,27 +4,38 @@ import pickle
 import pandas as pd
 
 # Load the pre-trained model
-with open('C:/Workspace/Pharmani/models/salary_model.pkl', 'rb') as f:
+with open('salary_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 # Load the encoders
-with open('C:/Workspace/Pharmani/models/job_role_encoder.pkl', 'rb') as f:
+with open('job_role_encoder.pkl', 'rb') as f:
     job_role_encoder = pickle.load(f)
 
-with open('C:/Workspace/Pharmani/models/work_location_encoder.pkl', 'rb') as f:
+with open('work_location_encoder.pkl', 'rb') as f:
     work_location_encoder = pickle.load(f)
 
 # Read data from stdin
 json_data = sys.stdin.read()
-data = json.loads(json_data)
-jobRole = [item['jobRole'] for item in data]
-workLocation = [item['workLocation'] for item in data]
+
+try:
+    data = json.loads(json_data)
+except json.JSONDecodeError:
+    print(json.dumps({"error": "Invalid JSON"}))
+    sys.exit(1)
+
+try:
+    jobRole = data['jobRole']
+    workLocation = data['workLocation']
+except KeyError:
+    print(json.dumps({"error": "Invalid input format. Expected fields: jobRole, workLocation"}))
+    sys.exit(1)
 
 # Perform label encoding
-jobRole_encoded = job_role_encoder.transform(jobRole)
-workLocation_encoded = work_location_encoder.transform(workLocation)
+jobRole_encoded = job_role_encoder.transform([jobRole])
+workLocation_encoded = work_location_encoder.transform([workLocation])
 
 # Perform the prediction
 df = pd.DataFrame(list(zip(jobRole_encoded, workLocation_encoded)), columns=['jobRole', 'workLocation'])
-predictions = model.predict(df)
-print(predictions)
+prediction = model.predict(df)
+
+print(json.dumps({"predictedSalary": prediction.tolist()[0]}))
